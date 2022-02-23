b41172a38fcd9203608a44c45d0e077432163b0c
Emmanuel Ndangurura <endangurura@tachyinc.com>
2022-02-23 16:32:56 +0300

    add bitcoin mainnet

--- src/cli.rs
+++ src/cli.rs
@@ -86,8 +86,9 @@ pub(crate) fn parse_startup_args() -> Result<LdkUserInfo, ()> {
 		Some("testnet") => Network::Testnet,
 		Some("regtest") => Network::Regtest,
 		Some("signet") => Network::Signet,
+		Some("bitcoin") => Network::Bitcoin,
 		Some(net) => {
-			panic!("Unsupported network provided. Options are: `regtest`, `testnet`, and `signet`. Got {}", net);
+			panic!("Unsupported network provided. Options are: `regtest`, `testnet`, `signet` and 'bitcoin`. Got {}", net);
 		}
 		None => Network::Testnet,
 	};
